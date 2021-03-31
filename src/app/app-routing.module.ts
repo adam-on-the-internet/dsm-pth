90e2bbb177e60ef16a26026dcbe5b2a53f5dbdb3
Adam Callanan <a.callanan@sbcglobal.net>
2021-03-30 22:23:04 -0500

    some more cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,6 @@ import {ROUTES_ENUM} from "./constants/routing.constants";
 import {LoginComponent} from "./components/login/login.component";
 import {ProfileComponent} from "./components/profile/profile.component";
 import {AdminComponent} from "./components/admin/admin.component";
-import {BookmarkOverviewComponent} from "./components/bookmark-overview/bookmark-overview.component";
-import {BookmarkFormComponent} from "./components/bookmark-form/bookmark-form.component";
-import {BookmarkDetailsComponent} from "./components/bookmark-details/bookmark-details.component";
-import {BookmarksComponent} from "./components/bookmarks/bookmarks.component";
-import {StageComponent} from "./components/stage/stage.component";
-import {LinkComponent} from "./components/link/link.component";
-import {MonitorComponent} from "./components/monitor/monitor.component";
-import {SuperlativeScavengerComponent} from "./components/superlative-scavenger/superlative-scavenger.component";
-import {MuppetListingComponent} from "./components/muppet-listing/muppet-listing.component";
-import {MuppetDisplayComponent} from "./components/muppet-display/muppet-display.component";
-import {MuppetFormComponent} from "./components/muppet-form/muppet-form.component";
-import {MuppetRandomComponent} from "./components/muppet-random/muppet-random.component";
-import {MuppetStatsComponent} from "./components/muppet-stats/muppet-stats.component";
 
 const routes: Routes = [
   // main
@@ -27,23 +14,6 @@ const routes: Routes = [
   {path: ROUTES_ENUM.Login, component: LoginComponent},
   {path: ROUTES_ENUM.Profile, component: ProfileComponent},
   {path: ROUTES_ENUM.Admin, component: AdminComponent},
-  // projects
-  {path: ROUTES_ENUM.Muppet, component: MuppetListingComponent},
-  {path: ROUTES_ENUM.MuppetStats, component: MuppetStatsComponent},
-  {path: ROUTES_ENUM.MuppetRandom, component: MuppetRandomComponent},
-  {path: ROUTES_ENUM.Muppet, component: MuppetListingComponent},
-  {path: ROUTES_ENUM.Muppet + "/:id", component: MuppetDisplayComponent},
-  {path: ROUTES_ENUM.MuppetForm, component: MuppetFormComponent},
-  {path: ROUTES_ENUM.MuppetForm + "/:id", component: MuppetFormComponent},
-  {path: ROUTES_ENUM.Bookmarks, component: BookmarksComponent},
-  {path: ROUTES_ENUM.BookmarkOverview, component: BookmarkOverviewComponent},
-  {path: ROUTES_ENUM.BookmarkOverview + "/:id", component: BookmarkDetailsComponent},
-  {path: ROUTES_ENUM.BookmarkForm, component: BookmarkFormComponent},
-  {path: ROUTES_ENUM.BookmarkForm + "/:id", component: BookmarkFormComponent},
-  {path: ROUTES_ENUM.Stage, component: StageComponent},
-  {path: ROUTES_ENUM.Links, component: LinkComponent},
-  {path: ROUTES_ENUM.Monitor, component: MonitorComponent},
-  {path: ROUTES_ENUM.SuperlativeScavenger, component: SuperlativeScavengerComponent},
   // default
   {path: "**", redirectTo: "dashboard"},
 ];
