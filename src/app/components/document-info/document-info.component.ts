eef3e7d80f69a20b11c8ec3e52bc5b3a652261d7
Adam Callanan <a.callanan@sbcglobal.net>
2021-05-08 20:36:49 -0500

    cleanup for mobile, adding pdf viewer

--- src/app/components/document-info/document-info.component.ts
+++ src/app/components/document-info/document-info.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {DOCUMENTS} from "../../constants/document-constants";
 import {DocumentInfo} from "../../models/DocumentInfo.model";
+import {DomSanitizer, SafeResourceUrl, SafeUrl} from "@angular/platform-browser";
 
 @Component({
   selector: "app-document-info",
@@ -19,8 +20,21 @@ export class DocumentInfoComponent implements OnInit {
     return this.doc === undefined;
   }
 
+  public get src(): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.link);
+  }
+
+  public get href(): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(this.link);
+  }
+
+  private get link(): string {
+    return `/assets/${this.doc.link}`;
+  }
+
   constructor(
     private route: ActivatedRoute,
+    private sanitizer: DomSanitizer,
   ) {
   }
 
