9b5a7e61fe3ca2ce6f4728a261880b55557af811
Adam Callanan <a.callanan@sbcglobal.net>
2021-05-08 20:09:40 -0500

    more docs

--- src/app/constants/document-constants.ts
+++ src/app/constants/document-constants.ts
@@ -3,16 +3,26 @@ import {DocumentInfo} from "../models/DocumentInfo.model";
 export const DOCUMENTS: DocumentInfo[] = [
   {
     name: "DMPS Student Resource Officer Report",
-    link: "SROReport-1.22.21.pdf",
+    link: "sro-report.pdf",
     description: "A report on Student Resource Officers (SROs) in schools. This was presented to Des Moines Public Schools who went on to remove SROs from schools.",
     date: "01/22/21",
-    // DMPD presentation about removing SROs from school
-    // school board (Jake Troja) was not planning on releasing this doc since it was presented to the school board already
   },
   {
     name: "Marijuana Task Force Recommendations / Report",
-    link: "mtf_report.pdf",
+    link: "mtf-report.pdf",
     description: "The final report from the 2020 DSM Marijuana Task Force.",
-    date: "11/09/21",
+    date: "11/09/20",
+  },
+  {
+    name: "DMPD Gold Braid Union Threatening Letter",
+    link: "gold-braid-letter.pdf",
+    description: "Threatening letter sent by a DMPD cop 'union' to Council after Josh Mandelbaum raised questions about DMPD Conduct. Contains lies referencing to events that never happened including a story that someone threw poop at cops.",
+    date: "08/26/20",
+  },
+  {
+    name: "Procedural Rules of DSM City Council",
+    link: "rules-as-of-2020.pdf",
+    description: "The rules of DSM City Council. NOTE: the rules do not reflect how meetings are actually run.",
+    date: "as amended on 11/09/20",
   },
 ];
