78b9bc573320c9379ced7a78f661e2d978582c2f
Adam Callanan <a.callanan@sbcglobal.net>
2021-12-05 14:34:41 -0600

    history rules only for admin

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import {Component} from "@angular/core";
+import {CookieHelper} from "../../utilities/cookie.util";
 
 @Component({
   selector: "app-rules",
   templateUrl: "./rules.component.html",
   styleUrls: ["./rules.component.scss"]
 })
-export class RulesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RulesComponent {
+  public get isAdmin(): boolean {
+    return CookieHelper.admin;
   }
-
 }
