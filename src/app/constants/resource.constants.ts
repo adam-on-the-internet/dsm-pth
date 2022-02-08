f1e51e30d136519ed785901f925f6a5d17ff8a35
Adam Callanan <a.callanan@sbcglobal.net>
2022-02-08 10:55:23 -0600

    add more resources

--- src/app/constants/resource.constants.ts
+++ src/app/constants/resource.constants.ts
@@ -29,4 +29,12 @@ export const RESOURCES: ResourceItem[] = [
     text: "Phone Number for Broadlawns Mobile Crisis Unit",
     details: "(515) 282-5752",
   },
+  {
+    text: "Des Moines: Find Your Precinct",
+    details: "https://www.google.com/url?q=https://www.google.com/maps/d/u/1/viewer?mid%3D1-WeGDfNxA9AbpbWmd91HuUQG-jBJIPqE%26ll%3D41.56896508616821%252C-93.72204921276854%26z%3D13&sa=D&source=editors&ust=1643847758092024&usg=AOvVaw005PnizLLikw2KCJDdgQIx"
+  },
+  {
+    text: "Des Moines: Find Your House District",
+    details: "https://www.google.com/url?q=https://iowa-legis.maps.arcgis.com/apps/instant/lookup/index.html?appid%3Dd32118cf09ed47beacd12742c1cf3fb3&sa=D&source=editors&ust=1643847758091459&usg=AOvVaw3Y_rM5CXS-sCUbrn_TSIqd\n",
+  },
 ];
