daa9bbd6d31ee9524ea454d760a9f6d7e4bbd0d2
Adam Callanan <a.callanan@sbcglobal.net>
2022-02-20 21:10:04 -0600

    newsletter page

--- src/app/components/newsletter/newsletter.component.ts
+++ src/app/components/newsletter/newsletter.component.ts
@@ -1,16 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.css']
 })
-export class NewsletterComponent implements OnInit {
+export class NewsletterComponent {
+  public allNewsletters = [
+    {
+      link: "assets/newsletter/peoples_gazette_february_2022.pdf",
+      name: "People's Gazette: February 2022",
+      details: "Our first Newsletter, featuring information about Community Fridges, Council Donors, and more.",
+      released: "February 21, 2022"
+    },
+  ];
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
+  public get latestNewsletter() {
+    return this.allNewsletters[0];
   }
 
 }
