461afa0ebb472846e21bf06ba9c925cb0755af26
Adam Callanan <a.callanan@sbcglobal.net>
2022-11-15 13:58:33 -0600

    update web form

--- src/app/components/mailto-generator/mailto-generator.component.ts
+++ src/app/components/mailto-generator/mailto-generator.component.ts
@@ -41,9 +41,7 @@ Account from Anamosa: https://drive.google.com/file/d/1BL-J9XkCEpWywmFNH9ppZ5nlt
 
 <p>
 The result of the new changes has been a severe restriction on incarcerated people’s access to communication with friends and family on the outside. This has had further detrimental impacts on the mental health of both incarcerated people and the many people trying to get in touch with loved ones on the other side of the prison walls. Pushing people away from their family and friends does not bring safety and security to people in prisons or outside communities. These changes further propel a system which strips away dignity and humanity, preventing any chance of healing and rehabilitation. In response, we raise forth these demands on behalf of incarcerated people in Iowa as a continuation of the
-
 demands issued in January 2021
-
 about the DoC’s mishandling of the COVID-19 pandemic, many of which are issues which continue to persist today:
 </p>
 <p>
@@ -51,89 +49,77 @@ Previous 2021 Demands: https://docs.google.com/document/d/1nfT27BuXFHvqC1NzPdeU5
 </p>
 <p>--------</p>
 <p>
-
 1. Decarceration. Minimize the number of individuals being arrested. Parole boards must release people and the governor must commute sentences in order to decrease the prison/jail population enough to allow for safety protocols.
-
 </p>
 <p>
-
 2. Suspend ICE enforcement and release Immigrants in ICE custody--especially individuals that are most vulnerable to the virus. The majority of people being detained by ICE are no danger to the community.
-
 </p>
 <p>--------</p>
 <p>
-
 Communication Demands:
-
 </p>
 <p>
-
 1. Reinstate the prior visitation policy for in-person visits of five days a week, eight times per person per month, with visitors able to come during visiting hours when convenient for them. Weekend visits should not count as “special visits,” but rather as two of the eight allotted visits.
-
+</p>
+<p>
 With the new policy, each individual is only allowed one in-person visit and one video visit per week, which is a huge burden on people with large families and support networks, especially for family and friends who visit from out of town.
 </p>
 <p>
-
 2. Eliminate strip searches and visitation room clothing for video visits.
-
+</p>
+<p>
 There is no security risk in conducting a video call, and therefore no need to strip search people prior to these visits. The required visitation clothing, which is already worn out and undignifying to wear, leaves few clean clothes for people that have in person visits. This is dehumanizing and unnecessary.
 </p>
 <p>
-
 3. Eliminate the need to sign up for visitations on the Ameelio app in advance.
-
+</p>
+<p>
 It is difficult for visitors to find time slots that work for their schedules and to plan visits less than one week in advance, and not all visitors have access to a smart device to sign up through the app.
 </p>
 <p>
-
 4. Allow video visits to occur on all weekdays and return to the Google Hangouts platform where multiple people can join the same call from different locations.
-
+</p>
+<p>
 Currently video visits can only be scheduled on Mondays and Fridays with one visitor at a time (unless visitors call in from the same location).
 </p>
 <p>
-
 5. Ensure that snail mail and photographs arrive in a timely manner (less than two weeks delivery) with no additional financial barriers besides postage.
-
+</p>
+<p>
 Currently the mail system through Pigeonly takes anywhere from two weeks to multiple months for mail to be delivered to people in prison.
 </p>
 <p>
-
 6. Ensure that any copies of photographs received by mail are of good or original quality.
-
+</p>
+<p>
 Scanned copies of photos on printer paper make images appear blurry, and they are often folded up and creased down the middle when received.
 </p>
 <p>--------</p>
 <p>
-
 Additional Demands:
-
 </p>
 <p>
-
 1. Eliminate requirement of wearing wristbands to indicate tip level.
-
+</p>
+<p>
 Incarcerated people in Iowa already have colored stickers on their ID cards so that staff can check their levels. Wearing wristbands is unnecessary and dehumanizing.
 </p>
 <p>
-
 2. Pay all incarcerated workers a living wage.
-
+</p>
+<p>
 Prices of commissary products have doubled, and even tripled in the past months with no increases in wages for incarcerated workers.
 </p>
 <p>
-
 3. Provide adequate mental health services and resources for all incarcerated people who want or need them, and ensure these services are available at all times.
-
 </p>
 <p>
-
 4. Allow incarcerated people to access all media and music purchased on their tablets if the state switches to a new vendor.
-
 </p>
 <p>
-
 5. Clarify the specific parameters and guidelines used to determine whether or not an individual’s commutation will be approved by the governor.
-
+</p>
+<p>
 Since beginning her term in office, the governor has denied every commutation request, regardless of the parole board’s recommendation on the basis of criminal record and severity of crime, conditions which cannot be changed by an individual making efforts at character improvement. In order for this process to be useful at all, individuals need to know exactly what they need to do in order to successfully receive a sentence commutation.
 </p>
 
